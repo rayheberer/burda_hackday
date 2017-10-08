a2f0e15a2c36b9e6a2afecfa3a2d7675063e81af
Kapil Bakshi <kapilbakshi88@gmail.com>
2017-10-08 12:28:16 +0200

    Code Refactoring

--- app/src/main/java/com/burdahackday/ui/TimeLineActivity.java
+++ app/src/main/java/com/burdahackday/ui/TimeLineActivity.java
@@ -1,5 +1,8 @@
 package com.burdahackday.ui;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
@@ -7,13 +10,18 @@ import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.burdahackday.model.Orientation;
 import com.burdahackday.model.Post;
 import com.burdahackday.model.PostsDatabaseHelper;
 import com.burdahackday.model.TimeLineModel;
+import com.burdahackday.model.User;
+import com.burdahackday.utils.DateTimeUtils;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.List;
 
 
@@ -24,6 +32,8 @@ public class TimeLineActivity extends AppCompatActivity {
     private List<TimeLineModel> mDataList = new ArrayList<>();
     private Orientation mOrientation;
     private boolean mWithLinePadding;
+    ProgressDialog progressDialog;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,6 +43,8 @@ public class TimeLineActivity extends AppCompatActivity {
         /*Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);*/
 
+
+
         if(getSupportActionBar()!=null)
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
@@ -41,6 +53,12 @@ public class TimeLineActivity extends AppCompatActivity {
 
         setTitle("Transformation Timeline");
 
+        progressDialog = new ProgressDialog(TimeLineActivity.this);
+        progressDialog.setMessage("Processing Please Wait !");
+        progressDialog.setProgressStyle(ProgressDialog.STYLE_SPINNER);
+        progressDialog.setIndeterminate(true);
+        progressDialog.show();
+
 /*
         setTitle(mOrientation == Orientation.HORIZONTAL ? getResources().getString(R.string.horizontal_timeline) : getResources().getString(R.string.vertical_timeline));
 */
@@ -48,7 +66,26 @@ public class TimeLineActivity extends AppCompatActivity {
         mRecyclerView = (RecyclerView) findViewById(R.id.recyclerView);
         mRecyclerView.setLayoutManager(getLinearLayoutManager());
         mRecyclerView.setHasFixedSize(true);
+        mRecyclerView.setVisibility(View.GONE);
 
+        new Handler().postDelayed(new Runnable() {
+
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+
+                mRecyclerView.setVisibility(View.VISIBLE);
+                progressDialog.dismiss();
+
+
+            }
+        }, 3000);
         initView();
     }
 
@@ -75,15 +112,7 @@ public class TimeLineActivity extends AppCompatActivity {
             mDataList.add(new TimeLineModel(post.mImageUrl, post.mDate, "20%"));
 
         }
-        /*mDataList.add(new TimeLineModel("Item successfully delivered", "", "20%"));
-        mDataList.add(new TimeLineModel("Courier is out to delivery your order", "2017-02-12 08:00", "20%"));
-        mDataList.add(new TimeLineModel("Item has reached courier facility at New Delhi", "2017-02-11 21:00", "20%"));
-        mDataList.add(new TimeLineModel("Item has been given to the courier", "2017-02-11 18:00", OrderStatus.COMPLETED));
-        mDataList.add(new TimeLineModel("Item is packed and will dispatch soon", "2017-02-11 09:30", OrderStatus.COMPLETED));
-        mDataList.add(new TimeLineModel("Order is being readied for dispatch", "2017-02-11 08:00", OrderStatus.COMPLETED));
-        mDataList.add(new TimeLineModel("Order processing initiated", "2017-02-10 15:00", OrderStatus.COMPLETED));
-        mDataList.add(new TimeLineModel("Order confirmed by seller", "2017-02-10 14:30", OrderStatus.COMPLETED));
-        mDataList.add(new TimeLineModel("Order placed successfully", "2017-02-10 14:00", OrderStatus.COMPLETED));*/
+
     }
 
     @Override
